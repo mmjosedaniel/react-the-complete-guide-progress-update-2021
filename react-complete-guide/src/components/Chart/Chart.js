1fb3958f21ba9297e6476b01f57abbd289c610c6
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-05-21 20:38:42 -0500

    70. Wrap Up and Next Steps

--- react-complete-guide/src/components/Chart/Chart.js
+++ react-complete-guide/src/components/Chart/Chart.js
@@ -2,13 +2,15 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximun = Math.max(...dataPointValues);
   return (
-    <div className="">
+    <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximun}
           label={dataPoint.label}
         />
       ))}
