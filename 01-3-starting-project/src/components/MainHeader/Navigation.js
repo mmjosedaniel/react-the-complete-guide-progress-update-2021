9df50273a9a98198dae5bfb32d653cd8251a05a4
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-06-26 08:06:09 -0500

    123. Making Context Dynamic

--- 01-3-starting-project/src/components/MainHeader/Navigation.js
+++ 01-3-starting-project/src/components/MainHeader/Navigation.js
@@ -3,8 +3,8 @@ import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
-const Navigation = (props) => {
-  const ctx = useContext(AuthContext)
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
       <ul>
@@ -20,7 +20,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
